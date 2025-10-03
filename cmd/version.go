ad79cf851113cbec2cc903368c79b58490541f43
allanice001 <allanice001@gmail.com>
2025-10-03 19:07:05 +0100

    fixing version info displayed

--- cmd/version.go
+++ cmd/version.go
@@ -2,8 +2,10 @@ package cmd
 
 import (
 	"fmt"
+	rdebug "runtime/debug"
+	"strings"
 
-	"github.com/earthboundkid/versioninfo/v2"
+	"github.com/DragonSecurity/github-org-manager-go/internal/version"
 	"github.com/spf13/cobra"
 )
 
@@ -11,10 +13,43 @@ var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Version:", versioninfo.Version)
-		fmt.Println("Revision:", versioninfo.Revision)
-		fmt.Println("DirtyBuild:", versioninfo.DirtyBuild)
-		fmt.Println("LastCommit:", versioninfo.LastCommit)
+		fmt.Println("Version:", version.Version)
+
+		// Optional: print VCS info if the build has it
+		if info, ok := rdebug.ReadBuildInfo(); ok {
+			var rev, t, dirty string
+			for _, s := range info.Settings {
+				switch s.Key {
+				case "vcs.revision":
+					rev = s.Value
+					if len(rev) > 12 {
+						rev = rev[:12]
+					}
+				case "vcs.time":
+					t = s.Value
+				case "vcs.modified":
+					if s.Value == "true" {
+						dirty = "dirty"
+					} else {
+						dirty = "clean"
+					}
+				}
+			}
+			if rev != "" || t != "" {
+				if rev == "" {
+					rev = "unknown"
+				}
+				if t == "" {
+					t = "unknown"
+				}
+				if dirty == "" {
+					dirty = "unknown"
+				}
+				fmt.Printf("Revision: %s\n", rev)
+				fmt.Printf("DirtyBuild: %s\n", strings.ToLower(dirty))
+				fmt.Printf("LastCommit: %s\n", t)
+			}
+		}
 	},
 }
 
